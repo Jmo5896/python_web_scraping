5d033e49156deeed5d813671a017497a93090447
Jmo5896 <jmo5896@moore-clan.net>
2019-09-11 12:35:45 -0600

    I think I have everything in place to launch on heroku

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-
+from login_db import myKeys
+keys = myKeys()
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = f"mongodb://{keys['userName']}:{keys['password']}@ds011268.mlab.com:11268/heroku_p589mnvp"
+
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route('/')
